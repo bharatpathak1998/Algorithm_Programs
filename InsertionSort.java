576a3733e6b11021328f6afa45571e9e63c86645
bharatpathak1998 <bharatpathak099@gmail.com>
2022-09-07 00:07:42 +0530

    Added : To Print The Sorted List

---
+++ InsertionSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {56, 25, 78, -2, 12, 29, 28, -41};
+        System.out.println(Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
